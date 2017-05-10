79c4efcb516b010e4d44c3e978d6b5ded8ba4c76
vast <1264188640@qq.com>
2017-05-10 13:43:57 +0800

    nodejs 博客

--- models/posts.js
+++ models/posts.js
@@ -3,6 +3,7 @@
  */
 var Post=require('../lib/mongo').Post
 var marked=require('marked')
+var CommentModel=require('./comment')
 
 // 将 post 的 content 从 markdown 转换成 html
 Post.plugin('contentToHtml',{
@@ -20,6 +21,27 @@ Post.plugin('contentToHtml',{
     }
 })
 
+// 给 post 添加留言数 commentsCount
+Post.plugin('addCommentsCount', {
+    afterFind: function (posts) {
+        return Promise.all(posts.map(function (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (commentsCount) {
+                post.commentsCount = commentsCount;
+                return post;
+            });
+        }));
+    },
+    afterFindOne: function (post) {
+        if (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (count) {
+                post.commentsCount = count;
+                return post;
+            });
+        }
+        return post;
+    }
+});
+
 module.exports={
     //创建文章
     create:function (post) {
@@ -30,6 +52,7 @@ module.exports={
         return Post.findOne({_id:postId})
             .populate({path:'author',model:'User'}) //关联Post模型的‘author’到User 模型（集合）
             .addCreatedAt()
+            .addCommentsCount()
             .contentToHtml()
             .exec();
     },
@@ -47,10 +70,33 @@ module.exports={
             .exec();
     },
     // 通过文章 id 给 pv 加 1
-    inPv:function (postId) {
+    incPv:function (postId) {
         return Post.update({_id:postId},{$inc:{pv:1}})
             .exec();
+    },
+    // 通过文章 id 获取一篇原生文章（编辑文章）
+    getRawPostById:function (postId) {
+        return Post
+            .findOne({_id:postId})
+            .populate({path:'author',model:'User'})
+            .exec()
+    },
+    // 通过用户 id 和文章 id 更新一篇文章
+    updatePostById:function (postId,author,data) {
+        return Post.update({_id:postId,author:author},{$set:data}).exec()
+    },
+    // 通过用户 id 和文章 id 删除一篇文章
+    delPostById: function delPostById(postId, author) {
+        return Post.remove({ author: author, _id: postId })
+            .exec()
+            .then(function (res) {
+                // 文章删除后，再删除该文章下的所有留言
+                if (res.result.ok && res.result.n > 0) {
+                    return CommentModel.delCommentsByPostId(postId);
+                }
+            });
     }
+
 
 //     我们使用了 markdown 解析文章的内容，所以在发表文章的时候可使用 markdown 语法（如插入链接、图片等等），关于 markdown 的使用请参考： Markdown 语法说明。
 // 我们在 PostModel 上注册了 contentToHtml，而 addCreatedAt 是在 lib/mongo.js 中 mongolass 上注册的
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@
 var express = require('express')
 var router = require('express').Router()
 
+var CommentModel =require('../models/comment')
 var PostModel = require('../models/posts')
 var checkLogin = require('../middlewares/check').checkLogin
 
@@ -63,33 +64,108 @@ router.get('/create', checkLogin, function (req, res, next) {
 })
 
 // GET /posts/:postId 单独一篇的文章页
-router.get('/:postId', checkLogin, function (req, res, next) {
-    res.send(req.flash())
-})
+router.get('/:postId', function(req, res, next) {
+    var postId = req.params.postId;
+
+    Promise.all([
+        PostModel.getPostById(postId),// 获取文章信息
+        CommentModel.getComments(postId),// 获取该文章所有留言
+        PostModel.incPv(postId)// pv 加 1
+    ])
+        .then(function (result) {
+            var post = result[0];
+            var comments = result[1];
+            if (!post) {
+                throw new Error('该文章不存在');
+            }
+
+            res.render('post', {
+                post: post,
+                comments: comments
+            });
+        })
+        .catch(next);
+});
 
 // GET /posts/:postId/edit 更新文章页
 router.get('/:postId/edit', checkLogin, function (req, res, next) {
-    res.send(req.flash())
+    var postId=req.params.postId
+    var author=req.session.user._id
+
+    PostModel.getRawPostById(postId)
+        .then(function (post) {
+            if(!post){
+                throw new Error("该文章不存在")
+            }
+            if(author.toString()!==post.author._id.toString()){
+                throw new Error("权限不足")
+            }
+            res.render('edit',{post:post})
+        })
+        .catch(next)
 })
 
 // POST /posts/:postId/edit 更新一篇文章
 router.post('/:postId/edit', checkLogin, function (req, res, next) {
-    res.send(req.flash())
+    var title=req.fields.title
+    var content=req.fields.content
+    var author=req.session.user._id
+    var postId=req.params.postId
+
+    PostModel.updatePostById(postId,author,{title:title,content:content})
+        .then(function () {
+            req.flash("success",'编辑文章成功')
+            res.redirect('/posts/'+postId)
+        })
+        .catch(next)
 })
 
 // GET /posts/:postId/remove 删除一篇文章
 router.get('/:postId/remove', checkLogin, function (req, res, next) {
-    res.send(req.flash());
+    var postId=req.params.postId
+    var author=req.session.user._id
+
+    PostModel.delPostById(postId,author)
+        .then(function () {
+            req.flash("success","删除成功")
+            //调到主页
+            res.redirect('/posts')
+        })
+        .catch(next)
 });
 
 // POST /posts/:postId/comment 创建一条留言
 router.post('/:postId/comment', checkLogin, function (req, res, next) {
-    res.send(req.flash());
+    var author = req.session.user._id;
+    var postId = req.params.postId;
+    var content = req.fields.content;
+    var comment = {
+        author: author,
+        postId: postId,
+        content: content
+    };
+
+    CommentModel.create(comment)
+        .then(function () {
+            req.flash('success', '留言成功');
+            // 留言成功后跳转到上一页
+            res.redirect('back');
+        })
+        .catch(next);
 });
 
 // GET /posts/:postId/comment/:commentId/remove 删除一条留言
 router.get('/:postId/comment/:commentId/remove', checkLogin, function (req, res, next) {
-    res.send(req.flash());
+    var commentId = req.params.commentId;
+    var author = req.session.user._id;
+
+    CommentModel.delCommentById(commentId, author)
+        .then(function () {
+            req.flash('success', '删除留言成功');
+            // 删除成功后跳转到上一页
+            res.redirect('back');
+        })
+        .catch(next);
 });
 
 module.exports = router
